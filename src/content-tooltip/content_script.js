ad0709e823e8a654799993561d86f718f369e626
digi0ps <sriru1998@gmail.com>
2018-05-21 22:22:55 +0530

    Add tooltip option to Settings.
    - Some tweaks to tooltip

--- src/content-tooltip/content_script.js
+++ src/content-tooltip/content_script.js
@@ -1,8 +1,17 @@
 import { bodyLoader } from 'src/util/loader'
+import { getLocalStorage } from 'src/util/storage'
 import * as interactions from './interactions'
 import { injectCSS } from 'src/search-injection/dom'
+import { TOOLTIP_STORAGE_NAME, TOOLTIP_DEFAULT_OPTION } from './constants'
 
 export async function init() {
+    const isTooltipEnabled = await getLocalStorage(
+        TOOLTIP_STORAGE_NAME,
+        TOOLTIP_DEFAULT_OPTION,
+    )
+
+    if (!isTooltipEnabled) return
+
     await bodyLoader()
 
     const target = document.createElement('div')
