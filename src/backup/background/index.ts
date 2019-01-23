1c0d8dbd1e8fcee1f8a2532436554811c5aa0996
Eik-S <eikem@posteo.de>
2019-01-23 18:57:17 +0100

    backup backend selection changed from compile time to users choice

--- src/backup/background/index.ts
+++ src/backup/background/index.ts
@@ -4,6 +4,7 @@ import { StorageManager } from '../../search/types'
 import { setupRequestInterceptors } from './redirect'
 import BackupStorage, { LastBackupStorage } from './storage'
 import { BackupBackend } from './backend'
+import { BackendSelect } from './backend-select'
 import estimateBackupSize from './estimate-backup-size'
 import BackupProcedure from './procedures/backup'
 import { BackupRestoreProcedure } from './procedures/restore'
@@ -14,9 +15,11 @@ export * from './backend'
 export class BackupBackgroundModule {
     storageManager: StorageManager
     storage: BackupStorage
+    backendLocation: string
     backend: BackupBackend
     lastBackupStorage: LastBackupStorage
-
+    changeTrackingQueue: Queue
+    backendSelect = new BackendSelect()
     backupProcedure: BackupProcedure
     backupUiCommunication = new ProcedureUiCommunication('backup-event')
     restoreProcedure: BackupRestoreProcedure
@@ -53,12 +56,12 @@ export class BackupBackgroundModule {
             storageManager,
             storage: this.storage,
             lastBackupStorage,
-            backend,
+            backend: this.backend,
         })
         this.restoreProcedure = new BackupRestoreProcedure({
             storageManager,
             storage: this.storage,
-            backend,
+            backend: this.backend,
         })
     }
 
@@ -116,11 +119,26 @@ export class BackupBackgroundModule {
                 hasInitialBackup: async () => {
                     return !!(await this.lastBackupStorage.getLastBackupTime())
                 },
+                setBackendLocation: async (info, location: string) => {
+                    if (
+                        location === 'gdrive' &&
+                        this.backendLocation !== location
+                    ) {
+                        this.backend = await this.backendSelect.initGDriveBackend()
+                    } else if (
+                        location === 'local' &&
+                        this.backendLocation !== location
+                    ) {
+                        this.backendLocation = location
+                        this.backend = await this.backendSelect.initLocalBackend()
+                    }
+                    this.setupRequestInterceptor()
+                },
                 isBackupAuthenticated: async () => {
-                    return this.backend.isAuthenticated()
+                    return this.backend ? this.backend.isAuthenticated() : false
                 },
                 isBackupConnected: async () => {
-                    return this.backend.isConnected()
+                    return this.backend ? this.backend.isConnected() : false
                 },
                 maybeCheckAutomaticBakupEnabled: async () => {
                     if (
@@ -172,6 +190,11 @@ export class BackupBackgroundModule {
             },
             { insertExtraArg: true },
         )
+    }
+
+    async setBackendFromStorage() {
+        this.backend = await this.backendSelect.restoreBackend()
+        return this.backend
     }
 
     setupRequestInterceptor() {
